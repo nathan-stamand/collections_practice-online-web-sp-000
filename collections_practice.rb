ba57d0a0276e93e60848fed06392205f55275c41
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-19 20:25:46 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,49 +1,72 @@
+require 'pry'
+
 def sort_array_asc(array)
   array.sort
-end 
+end
 
 def sort_array_desc(array)
-  array.sort do |a, b|
-    b <=> a 
+  array.sort do | left, right|
+    right <=> left
   end
 end
 
 def sort_array_char_count(array)
-  array.sort do |a, b|
-    a.length <=> b.length
+  array.sort do |left, right|
+    left.length <=> right.length
   end
 end
 
 def swap_elements(array)
-  ["blake", "scott", "ashley"]
-end 
+  array[1], array[2] = array[2], array[1]
+  array
+end
 
 def reverse_array(array)
-  array.reverse 
+  array.reverse
 end
 
 def kesha_maker(array)
-  ["bl$ke", "as$ley", "sc$tt"]
+  array.each do |item|
+    item[2] = "$"
+  end
 end
 
 def find_a(array)
-  ["apple", "avis", "arlo", "ascot"]
-end
-
-def add_s(array)
-  array.collect do  |i|
-    if i != "feet"
-      i << "s"
-    else
-      "feet"
-    end
+  array.find_all do |word|
+    word[0] == "a"
   end
+
+  # using select method
+    # array.select do |word|
+    #   word[0] == "a"
+    # end
 end
 
 def sum_array(array)
-  sum = 0 
-  array.each do |i|
-    sum += i 
+  sum = 0
+  array.each do |num|
+    sum+=num
   end
-  return sum
-end 
+  sum
+
+  # using reduce method
+    # array.reduce(:+)
+
+  # using inject method (short)
+     # array.inject(:+)
+
+  # using inject method (long)
+     # array.inject do |sum,x|
+     #  sum + x
+     # end
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
